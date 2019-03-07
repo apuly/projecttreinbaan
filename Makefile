bd114197bc9125b9c50f1c6f631315f25819fbb0
paul <paul@bersee.nl>
2019-03-07 13:54:09 +0000

    Reordered commands to be compatable with train driver, added treinbaan driver proces, added command for communication with hardware dependent software

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ CFLAGS = -Wall -Werror
 
 RM = rm -f
 
-SOURCES = */*.c driver/*/*.c
+SOURCES = */*.c driver/treinbaandriver/*.c
 HEADERS = includes/*.h
 OBJECTS = */*.o
 
--- includes/command.h
+++ includes/command.h
@@ -1,9 +1,11 @@
 #ifndef _COMMAND_H_
 #define _COMMAND_H_
 
-#define SET_ALPHA 0 /* set the alphabet  for a proces */
-#define SET_SENS  1 /* sets the senstitivity of a proces in the sync serv */
-#define REM_SENS  2 /* removes a sensitivity of a proces in the sync serv */
-#define SET_STATE 3 /* sets the state of a proces */
-#define EXIT      4 /* exit the program */
+#define SET_ALPHA  0 /* set the alphabet  for a proces */
+#define SET_SENS   1 /* sets the senstitivity of a proces in the sync serv */
+#define REM_SENS   2 /* removes a sensitivity of a proces in the sync serv */
+#define SET_STATE  3 /* sets the state of a proces */
+#define EXIT       4 /* exit the program */
+#define HDS_ACTION 5 /* send an action to execute to the hardware dependent
+                        software*/
 #endif /* _COMMAND_H_ */
