6f33afa98f4b1a091631a862de17e8f8437f57be
paul <paul@bersee.nl>
2019-03-13 14:09:37 +0000

    added rangeer algorithm proces

---
+++ includes/rangeer.h
@@ -0,0 +1,10 @@
+#ifndef _RANGEER_H_
+#define _RANGEER_H_
+
+#include "../includes/proces.h"
+
+#define NUM_RANGEER_SENS 0
+
+void rangeer_start(struct exec_data data);
+
+#endif /*_RANGEER_H_*/
---
+++ processes/rangeer_proces.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../includes/rangeer.h"
+#include "../includes/proces.h"
+#include "../includes/debug.h"
+
+#include "../includes/locomotief.h"
+#include "../includes/wissel.h"
+#include "../includes/ontkoppelaar.h"
+#include "../includes/sensor.h"
+#include "../includes/sync.h"
+#include "../includes/command.h"
+struct sens {
+  int proc;
+  int sys;
+  int sens;
+};
+
+void send_alpha(struct exec_data data, int proc_id, int num_procs, int alpha_c);
+struct sens update_sens(struct exec_data data, struct sens new_sens,
+                        struct sens cur_sens);
+void init_rangeer(struct exec_data data);
+struct sens set_sens(int p_id, int sys_id, int sens_id);
+
+
+void rangeer_start(struct exec_data data)
+{
+  int i = 0;
+  struct sens c_sens;/*the current sensitivity*/
+  init_rangeer(data);
+  
+  sleep(START_PROCES_WAIT_TIME);
+  c_sens = set_sens(WISSEL_PROCES, 0, KROM);
+  send_sync_cmd(data, SET_SENS, c_sens.proc, c_sens.sys, c_sens.sens);
+  while(1){
+    sleep(2);
+    c_sens = update_sens(data, set_sens(WISSEL_PROCES, 0, RECHT), c_sens);
+    sleep(2);
+    c_sens = update_sens(data, set_sens(WISSEL_PROCES, 0, KROM), c_sens);
+  }
+}
+
+struct sens update_sens(struct exec_data data, struct sens new_sens,
+                        struct sens cur_sens)
+{
+  send_sync_cmd(data, REM_SENS, cur_sens.proc, cur_sens.sys, cur_sens.sens);
+  send_sync_cmd(data, SET_SENS, new_sens.proc, new_sens.sys, new_sens.sens);
+  return new_sens;
+}
+
+struct sens set_sens(int p_id, int sys_id, int sens_id)
+{
+  struct sens s_;
+  s_.proc = p_id;
+  s_.sys = sys_id;
+  s_.sens = sens_id;
+  return s_;
+}
+
+void init_rangeer(struct exec_data data)
+{
+  int type, num_proc, num_sens;
+  /*iterate through all processes types, get their number of running processes,
+    get the number of sensitivities, and mark their entire alphabet*/
+  for(type=0; type<NUM_PROCES_TYPES; type++){
+    num_proc = get_num_procs(type);
+    num_sens = get_num_sens(type);
+    #if DEBUG_RANGEER
+    printf("%d: %d, %d\n", type, num_proc, num_sens);
+    #endif
+    send_alpha(data, type, num_proc, num_sens);
+  }
+}
+
+void send_alpha(struct exec_data data, int proc_id, int num_procs, int alpha_c)
+{
+  int sys_id, i;
+  for (sys_id=0; sys_id<num_procs; sys_id++){
+    for (i=0; i<alpha_c; i++){
+      send_sync_cmd(data, SET_ALPHA, proc_id, sys_id, i);
+    }
+  }
+}
